01e6f966f0860417fbfb1883cd3ec6e956b1ac13
Mahesh-Motale77 <motalemahesh77@gmail.com>
2024-06-13 16:09:01 +0530

    Object 1.1

---
+++ static/Object.java
@@ -0,0 +1,25 @@
+
+/*
+            *** Object ***
+
+- Object is a block of memory, which is created at runtime in heap area which is representation
+  real world entity.
+  
+NOTE:
+- To create any object or before creating an object in real world, a design or blue print
+  is created. It provides all the specification of an object.
+
+Ex. 
+- Before manufacturing a car, a blue print of car is created.
+- Similarly in java before creating an object, the blue print must be created, which contain all the specification of that Object.
+  This design is done with the help of class...
+
+
+
+
+ */
+
+
+public class Object {
+    
+}
