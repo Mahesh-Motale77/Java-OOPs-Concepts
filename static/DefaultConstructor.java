1eba5c49215415aa5709636b9e6dbbcae998400d
Mahesh-Motale77 <motalemahesh77@gmail.com>
2024-06-16 23:11:28 +0530

    Default Constructor 1.1

--- static/DefaultConstructor.java
+++ static/DefaultConstructor.java
@@ -1,5 +1,37 @@
 
+/*
 
-public class DefaultConstructor {
+- Whenever programmer not giving any constructor, Compiler will there own no argument constructor,
+ This is called Default Constructor.
+
+ */
+
+ 
+class Test{
+    String brand = "Mobile";
+    double price;
+    String color;
+    String name;
+ 
+    // Compiler will add there own default constructor
+    // Test(){   }
+ 
+    void Display(){
+       System.out.println(brand);
+       System.out.println(price);
+       System.out.println(color);
+       System.out.println(name);
+    }
     
-}
+ }
+ 
+ public class DefaultConstructor {
+    public static void main(String[] args) {
+       Test t1 = new Test();
+ 
+       t1.Display();
+    }  
+     
+ }
+ 
+
