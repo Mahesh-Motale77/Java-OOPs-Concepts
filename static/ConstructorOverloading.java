48f9f696c4656cb955b4c6941999e6049c063c38
Mahesh-Motale77 <motalemahesh77@gmail.com>
2024-06-19 16:58:25 +0530

    Constructor Overloading 1.1

---
+++ static/ConstructorOverloading.java
@@ -0,0 +1,37 @@
+
+/*          
+                *** Constructor Overloading ***
+
+- A class having more than one constructor is known as Constructor Overloading.
+
+Rules:
+- Names of the constructor must be same.
+- The formal arguments of the constructor, must changed either by its type or number of formal arguments.
+
+Purpose
+- We do constructor overloading when we want to customie the loading of the values to the object menmbers.
+
+
+ */
+
+
+class Form
+{
+    String name;
+    String location;
+    String email;
+    long phno;
+    String blood;
+    double height;
+    int weight;
+
+    Form(){
+        System.out.println("From no argument constructor...");
+    }
+
+    
+}
+
+public class ConstructorOverloading {
+    
+}
